b7ae374298d7b930eb9ae4f7b8d6679b1e06210d
unknown <muhammadedo017@gmail.com>
2024-01-15 15:57:16 +0700

    commit pertama

---
+++ sekuroday4/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/sekuroday4/src
+# Build directory: /root/sekuroday4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("demo_pkg")
---
+++ sekuroday4/build/demo_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/sekuroday4/src/demo_pkg
+# Build directory: /root/sekuroday4/build/demo_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
